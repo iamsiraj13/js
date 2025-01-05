bb4cde2d7b6bd7c7931605af0fec36bd74515be3
iamsiraj13 <iamsiraj13@gmail.com>
2025-01-05 09:48:16 +0600

    for loop

--- conditions.js
+++ conditions.js
@@ -1,8 +1,29 @@
-var a = 10;
-var b = 21;
+var date = new Date();
 
-if (b % 2 === 0) {
-  console.log("B is even number");
-} else {
-  console.log("B is odd number");
+// 0 - sunday, 1- monday, 2 - tuesday,
+
+var today = date.getDate();
+
+switch (today) {
+  case 0:
+    console.log("Today is Sunday");
+    break;
+  case 1:
+    console.log("Today is Monday");
+    break;
+  case 2:
+    console.log("Today is Tuesday");
+    break;
+  case 3:
+    console.log("Today is Wednesday");
+    break;
+  case 4:
+    console.log("Today is Thursday");
+    break;
+  case 5:
+    console.log("Today is Friday");
+    break;
+  case 6:
+    console.log("Today is Saturday");
+    break;
 }
---
+++ ternary.js
@@ -0,0 +1,7 @@
+var n = 336;
+var result = n % 2 === 0 ? "Even" : "Odd";
+console.log(result);
+
+var name = true;
+
+name && result;
