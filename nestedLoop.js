4f14f53f6da9009792d1c717c8439c020a12c330
iamsiraj13 <iamsiraj13@gmail.com>
2025-01-05 11:26:23 +0600

    patter

--- nestedLoop.js
+++ nestedLoop.js
@@ -1,1 +1,9 @@
-console.log("hello");
+let col = 10;
+
+for (let i = 1; i <= col; i++) {
+  var result = "";
+  for (let j = 1; j <= col; j++) {
+    result += "*" + " ";
+  }
+  console.log(result);
+}
