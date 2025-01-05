cff06beab76826ec68e6f4cf6692cbfff6113e51
iamsiraj13 <iamsiraj13@gmail.com>
2025-01-05 11:18:25 +0600

    nested loop

---
+++ dowhile.js
@@ -0,0 +1,6 @@
+let count = 7;
+let isRun = false;
+
+do {
+  console.log(7);
+} while (isRun);
--- loop.js
+++ loop.js
@@ -1,3 +1,19 @@
-for (let i = 1; i < 100; i++) {
-  console.log("Sirajul Islam");
+let sum = 0;
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    sum += i;
+  }
+}
+// console.log(sum);
+let isRun = true;
+
+while (isRun) {
+  var rand = Math.floor(Math.random() * 1000 + 100);
+  if (rand === 9) {
+    console.log("Winner");
+    isRun = false;
+  } else {
+    console.log(rand);
+  }
 }
