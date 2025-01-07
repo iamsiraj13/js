b6f5e6a2bbf252f9a04eaec4d14ce0dd7a53d85d
iamsiraj13 <iamsiraj13@gmail.com>
2025-01-07 12:16:16 +0600

    string method

--- string.js
+++ string.js
@@ -13,3 +13,43 @@ const compareValue = string1.localeCompare(string2);
 console.log(compareValue);
 
 console.log("azz" < "lAA");
+
+var fname = "I am";
+var lname = "Sirajul";
+
+var fullname = fname.concat(" ", lname);
+console.log(fullname);
+
+// substring
+
+var surname = fullname.substr(2, fullname.length);
+console.log(surname);
+
+console.log(surname.charAt(0));
+
+console.log(surname.startsWith("a")); // true
+
+console.log(surname.endsWith(3)); // false
+
+console.log(surname.toUpperCase());
+console.log(surname.toLowerCase());
+var str = "  sdjflksjdf   slkjdf   sdj   ";
+
+console.log(str.trim());
+var str2 = "bangladesh";
+//console.log(str2.split(" "));
+
+// how to get string length;
+
+function lengthCheck(str) {
+  let length = 0;
+  while (true) {
+    if (str.charAt(length) == "") {
+      break;
+    } else {
+      length++;
+    }
+  }
+  return length;
+}
+console.log(lengthCheck("mrsirajul"));
