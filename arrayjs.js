1bda72572db9d9c6596041c9634222901ed49f39
iamsiraj13 <iamsiraj13@gmail.com>
2025-01-07 17:56:15 +0600

    array manipulate

---
+++ arrayjs.js
@@ -0,0 +1,29 @@
+var arr = [4, 5, 6, 7, 8, 9];
+
+// odd numbe from array
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 !== 0) {
+    console.log(arr[i]);
+  }
+}
+// even numbe from array
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 === 0) {
+    console.log(arr[i]);
+  }
+}
+// sum of off array element
+let result = 0;
+for (let i = 0; i < arr.length; i++) {
+  result += arr[i];
+}
+console.log(result);
+// adding 2 with every item in array
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i] + 2);
+}
+
+// array reverce
+for (let i = arr.length - 1; i > 0; i--) {
+  console.log(arr[i]);
+}
