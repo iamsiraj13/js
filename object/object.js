4ed12919ff0a2b585449fade9d014b004911d087
iamsiraj13 <iamsiraj13@gmail.com>
2025-01-08 23:18:22 +0600

    ok

---
+++ object/object.js
@@ -0,0 +1,27 @@
+// objects
+
+/**
+ * student 1
+ * math: 80
+ * chemistry: 75
+ * physics: 56
+ * higher math: 90
+ */
+
+// [ 80, 75 , 86, 90 ]
+var obj = {
+  x: 10,
+};
+
+console.log(obj["x"]);
+
+obj.x = 20;
+console.log(obj);
+
+/*---------*/
+
+var obj2 = Object({
+  name: "sirajul",
+});
+obj2.x = 100;
+console.log(obj2);
