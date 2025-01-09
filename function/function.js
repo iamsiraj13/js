2c22c2dfa6ef3bbf64731616330eb4122f1e4230
iamsiraj13 <iamsiraj13@gmail.com>
2025-01-09 17:53:51 +0600

    function argument

--- function/function.js
+++ function/function.js
@@ -4,8 +4,12 @@
 var arr1 = [1, 2, 3];
 var arr2 = [45, 55, 12];
 var arr3 = [54, 39, 40];
+console.log(toString.call(arr1));
 
 function sum(arr) {
+  if (toString.call(arr) !== "[object Array]") {
+    return;
+  }
   let result = 0;
   for (elm of arr) {
     result += elm;
@@ -14,3 +18,15 @@ function sum(arr) {
 }
 
 sum(arr1);
+
+function addall() {
+  console.log(arguments);
+  var arr = Object.values(arguments);
+  var result = 0;
+  for (elm of arr) {
+    result += elm;
+  }
+  console.log(result);
+}
+
+addall(1, 2, 3, 4, 5, 6, 7);
