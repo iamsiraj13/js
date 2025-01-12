93a98a054a69b8d1b87d2676154113d586da81e6
iamsiraj13 <iamsiraj13@gmail.com>
2025-01-12 21:50:01 +0600

    counter 2

--- dsa/counter2.js
+++ dsa/counter2.js
@@ -1,3 +1,19 @@
-console.log("counter");
+function createCounter(init) {
+  let n = init;
+  return {
+    increment: function () {
+      return ++n;
+    },
+    decrement: function () {
+      return --n;
+    },
+    reset: function () {
+      return (n = init);
+    },
+  };
+}
 
-console.log("Hello world.");
+var counter = createCounter(10);
+console.log(counter.increment());
+console.log(counter.decrement());
+console.log(counter.reset());
