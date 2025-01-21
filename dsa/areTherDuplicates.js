c17430b99ab0f2e7bd1f635b0fdab708927d13e7
iamsiraj13 <iamsiraj13@gmail.com>
2025-01-21 23:19:47 +0600

    understanding multipointer

--- dsa/areTherDuplicates.js
+++ dsa/areTherDuplicates.js
@@ -8,30 +8,30 @@
  */
 
 // time complexity O(n)
-// function areThereAnyDuplicates(arr) {
-//   let i = 0;
-//   let j = 1;
-//   while (i < arr.length) {
-//     if (arr[i] === arr[j]) {
-//       return true;
-//     }
-//     i++;
-//     j++;
-//   }
-//   return false;
-// }
-
-// time complexity O(n^2)
 function areThereAnyDuplicates(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] === arr[j]) {
-        return true;
-      }
+  let i = 0;
+  let j = 1;
+  while (i < arr.length) {
+    if (arr[i] === arr[j]) {
+      return true;
     }
+    i++;
+    j++;
   }
   return false;
 }
 
-console.log(areThereAnyDuplicates([1, 1, 1, 1, 1, 1, 2]));
-console.log(areThereAnyDuplicates([1, 2, 3, 4, 5, 6, 7, 8]));
+// time complexity O(n^2)
+// function areThereAnyDuplicates(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] === arr[j]) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+
+console.log(areThereAnyDuplicates([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+// console.log(areThereAnyDuplicates([1, 2, 3, 4, 5, 6, 7, 8]));
