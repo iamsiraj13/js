34bb1ea2a6da2f1d58d647386ebf4dba89c6ae5b
iamsiraj13 <iamsiraj13@gmail.com>
2025-01-26 13:49:55 +0600

    strStr

---
+++ dsa/strStr.js
@@ -0,0 +1,35 @@
+/**
+ * Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0
+
+Example 2:
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ */
+
+const haystack = "sirajul";
+const needle = "jul";
+
+// const haystack = "leetcode";
+// const needle = "leeto";
+var strStr = function (haystack, needle) {
+  if (needle === 0) return 0;
+
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+    let j = 0;
+    for (j; j < needle.length; j++) {
+      if (haystack[i + j] !== needle[j]) break;
+    }
+    if (j === needle.length) return i;
+  }
+  return -1;
+};
+
+console.log(strStr(haystack, needle));
