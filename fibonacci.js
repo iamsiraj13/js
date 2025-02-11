97062e1806fbd1612da180fc08c6648f6c9f242e
iamsiraj13 <iamsiraj13@gmail.com>
2025-02-11 21:53:02 +0600

    fibonacci with recursion

--- fibonacci.js
+++ fibonacci.js
@@ -7,7 +7,14 @@ function fibonacci(n) {
   for (let i = 2; i <= n; i++) {
     result.push(result[i - 1] + result[i - 2]);
   }
-  return result;
+  return result[n];
 }
 
-console.log(fibonacci(23));
+console.log(fibonacci(15));
+
+function fibo(n) {
+  if (n <= 2) return 1;
+  return fibo(n - 1) + fibo(n - 2);
+}
+
+console.log(fibo(15));
