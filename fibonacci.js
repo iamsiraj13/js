982e75179c231f3a0e65cd6b59d848b02d555fa3
iamsiraj13 <iamsiraj13@gmail.com>
2025-02-11 23:39:06 +0600

    factorial

--- fibonacci.js
+++ fibonacci.js
@@ -7,7 +7,7 @@ function fibonacci(n) {
   for (let i = 2; i <= n; i++) {
     result.push(result[i - 1] + result[i - 2]);
   }
-  return result[n];
+  return result[n - 1];
 }
 
 console.log(fibonacci(15));
