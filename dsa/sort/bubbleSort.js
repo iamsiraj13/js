ba4c2d9a83b71ac16dbaceceb8164e3810b3e7ba
iamsiraj13 <iamsiraj13@gmail.com>
2025-02-15 17:23:43 +0600

    selection sort

--- dsa/sort/bubbleSort.js
+++ dsa/sort/bubbleSort.js
@@ -22,7 +22,7 @@
 // Big-O = O(n^2)
 
 // another way
-let myArray = [64, 34, 25, 12, 22, 11, 90, 5];
+let myArray = [64, 1, 34, 25, 12, 22, 11, 90, 5];
 
 let n = myArray.length;
 
@@ -34,16 +34,20 @@ let n = myArray.length;
 //     }
 //   }
 // }
+console.log(
+  myArray.sort((a, b) => {
+    return a - b;
+  })
+);
 
 function bubsort(arr) {
   console.log(arr);
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - i - 1; j++) {
-      console.log(arr, arr[i], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+        // let temp = arr[j];
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        // arr[j + 1] = temp;
       }
     }
   }
---
+++ dsa/sort/selectionSort.js
@@ -0,0 +1,17 @@
+let myArr = [1, 5, 2, 9, 6, 10, 8];
+
+function selSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    [arr[i], arr[lowest]] = [arr[lowest], arr[i]];
+  }
+
+  return arr;
+}
+
+console.log(selSort(myArr));
