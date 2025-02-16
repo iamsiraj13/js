fca7483231c0d8a722615945add7abefc24f3452
iamsiraj13 <iamsiraj13@gmail.com>
2025-02-16 23:01:33 +0600

    16 feb

--- dsa/sort/mergeSort.js
+++
@@ -1,8 +0,0 @@
-const arr1 = [5, 7, 12, 19];
-const arr2 = [2, 3, 8, 21];
-console.log(arr2.sort());
-
-function mergeSort(arr1, arr2) {
-  return arr1.concat(arr2).sort();
-}
-console.log(mergeSort(arr1, arr2));
---
+++ dsa/sort/mymerger.js
@@ -0,0 +1,13 @@
+function bubble() {
+  let arr = [1, 3, 4, 5, 56, 6];
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
