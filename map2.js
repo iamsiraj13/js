99a36cd6d72bedaf7343b23b639f2a165786607c
iamsiraj13 <iamsiraj13@gmail.com>
2025-02-18 20:10:38 +0600

    javascript map data structure

---
+++ map2.js
@@ -0,0 +1,8 @@
+let myMap = new Map();
+myMap.set(23, "sirajul");
+myMap.set(24, "tajmin");
+myMap.set(25, "sirajul");
+myMap.set(26, "sirajul");
+myMap.set(27, "sirajul");
+myMap.set(28, "sirajul");
+console.log(myMap.size);
