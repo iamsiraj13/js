2516c82f999d0ff5f84c54f33b8f61f84d24c83c
iamsiraj13 <iamsiraj13@gmail.com>
2025-03-12 16:14:18 +0600

    js es6 arrow function

--- dom/scripts.js
+++ dom/scripts.js
@@ -1,2 +1,12 @@
-let p = document.getElementById("demo")
-p.innerHTML = "Test case 01" 
+let p = document.getElementById("demo");
+let tag = document.getElementsByTagName("p");
+console.log(tag);
+console.dir(p);
+// p.innerHTML = "Loading";
+// setTimeout(function () {
+//   p.innerHTML = "Test case 01";
+// }, 3000);
+p.id = "sirajul";
+p.classList.value = "mang";
+p.outerText = "hello bondhu";
+console.log(p.classList);
---
+++ es6/function.js
@@ -0,0 +1,28 @@
+function num(x) {
+  return x * 10;
+}
+
+const num1 = (x) => {
+  return x * 10;
+};
+const num2 = (x) => x * 10;
+console.log(num(10));
+
+var javascript = {
+  name: "Javascript",
+  libraries: ["React", "Angular", "Vue"],
+  print: function () {
+    var self = this;
+    this.libraries.forEach(function () {
+      console.log(self.name);
+    });
+  },
+};
+
+console.log(javascript.print());
+
+function Person=(name)=> {
+  this.name = name;
+}
+var sakib = new Person("sakib");
+console.log(sakib);
