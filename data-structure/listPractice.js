4153b5f603f47994d140822c3ce34593cd5038e4
iamsiraj13 <iamsiraj13@gmail.com>
2025-03-19 16:01:53 +0600

    link

--- data-structure/listPractice.js
+++ data-structure/listPractice.js
@@ -11,16 +11,9 @@ class List {
     this.tail = null;
     this.length = 0;
   }
-  // checking list is empty or not
-
-  isEmpty() {
-    return this.length === 0;
-  }
-
-  // adding node end of the list
   push(value) {
     const node = new Node(value);
-    if (this.isEmpty()) {
+    if (!this.head) {
       this.head = node;
       this.tail = node;
     } else {
@@ -29,22 +22,9 @@ class List {
     }
     this.length++;
   }
-  // adding node start of the list
-  push(value) {
-    const node = new Node(value);
-    if (this.isEmpty()) {
-      this.head = node;
-      this.tail = node;
-    } else {
-      this.tail.next = node;
-      this.tail = node;
-    }
-    this.length++;
-  }
-  // adding node beggining of the list
   unshift(value) {
     const node = new Node(value);
-    if (this.isEmpty()) {
+    if (!this.head) {
       this.head = node;
       this.tail = node;
     } else {
@@ -58,18 +38,57 @@ class List {
       return null;
     }
     if (this.length === 1) {
-      let removeNode = this.head;
+      let removedNode = this.head;
       this.head = null;
       this.tail = null;
       this.length = 0;
-      return removeNode;
+      return removedNode;
+    }
+
+    let currentNode = this.head;
+    let lastNode = this.tail;
+    let newLastNode;
+    while (currentNode) {
+      if (currentNode.next === this.tail) {
+        newLastNode = currentNode;
+        break;
+      }
+      currentNode = currentNode.next;
+    }
+    newLastNode.next = null;
+    this.tail = currentNode;
+    this.length--;
+    return lastNode;
+  }
+  insertAtTail(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+
+  shift() {
+    if (!this.head) {
+      return;
+    }
+    let currentNode = this.head;
+    this.head = currentNode.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
     }
   }
 }
 
-let list = new List();
+let nodelist = new List();
+nodelist.push(10);
+nodelist.insertAtTail(100);
 
-list.push(100);
-list.push(500);
-list.pop();
-console.log(list);
+console.log(nodelist);
