5a46be9aa352496335510950ac46ee9efa653e1b
iamsiraj13 <iamsiraj13@gmail.com>
2025-04-13 17:43:31 +0600

    check duplicate from array

---
+++ basicProblem/duplicateInArray.js
@@ -0,0 +1,17 @@
+// function containDuplicate(nums) {
+//   const numSet = new Set(nums);
+//   console.log(numSet);
+//   for (const num of nums) {
+//     if (numSet.has(num)) {
+//       return true;
+//     }
+//     numSet.add(num);
+//   }
+//   return false;
+// }
+function containDuplicate(nums) {
+  return new Set(nums).size !== nums.length;
+}
+
+console.log(containDuplicate([1, 2, 3, 1, "sjk"]));
+console.log(containDuplicate([1, 2, 3, 4]));
