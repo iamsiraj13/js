77134d6274df61de4b90331d11a7f42731c2b742
iamsiraj13 <iamsiraj13@gmail.com>
2025-04-15 11:05:33 +0600

    is element exist in another array

--- dsa/isElementExists.js
+++ dsa/isElementExists.js
@@ -8,10 +8,24 @@ function isElementExists(arr1, arr2) {
   for (let elm of arr1) {
     if (arr2.includes(elm)) {
       return true;
-    } else {
-      return false;
     }
   }
+  return false;
 }
 
 console.log(isElementExists(arr1, arr2));
+
+function isExist(arr1, arr2) {
+  // flag variable
+  let exits = false;
+  for (let elm of arr1) {
+    if (arr2.includes(elm)) {
+      exits = true;
+    } else {
+      exits = false;
+    }
+  }
+  return exits;
+}
+
+console.log(isExist(arr1, arr2));
