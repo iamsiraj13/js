8f42228e20c08e72960bb095d5c5937990fa9e58
iamsiraj13 <iamsiraj13@gmail.com>
2025-04-16 11:19:40 +0600

    optimize two sum

--- dsa/twoSum.js
+++ dsa/twoSum.js
@@ -6,15 +6,30 @@
  *  time O(n)
  * space O(1)
  */
-function twoSum(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] === target) {
-        return [i, j];
-      }
+// function twoSum(arr, target) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// }
+
+// console.log(twoSum([9, 4, 8, 3, 2], 6));
+
+var twoSum = function (nums, target) {
+  const pairIdx = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let want = target - nums[i];
+    if (want in pairIdx) {
+      return [i, pairIdx[want]];
     }
+
+    pairIdx[nums[i]] = i;
   }
-}
+};
 
 console.log(twoSum([9, 4, 8, 3, 2], 6));
 
