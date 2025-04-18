1d09626ce396552f8c7affd4e37dcf0a7eea8b5a
iamsiraj13 <iamsiraj13@gmail.com>
2025-04-18 09:20:06 +0600

    is duplicate with multiple pointer

--- dsa/multiplePointer.js
+++ dsa/multiplePointer.js
@@ -26,18 +26,56 @@ function sumZero(arr) {
   let right = arr.length - 1;
 
   while (left < right) {
-    if (arr[left] + arr[right] === 0) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
       return [arr[left], arr[right]];
     }
-    if (arr[left] + arr[right] > 0) {
+    if (sum > 0) {
       right--;
     }
-    if (arr[left] + arr[right] < 0) {
+    if (sum < 0) {
       left++;
     }
   }
   return undefined;
 }
 // const arr = [-3, -2, -1, 0, 1, 2, 3];
-const arr = [-2, 0, 2, 3];``
+const arr = [-2, 0, 2, 3];
+``;
 console.log(sumZero(arr));
+
+function areThereDuplicate(arr) {
+  let hasmap = {};
+  for (let elm of arr) {
+    hasmap[elm] = (hasmap[elm] || 0) + 1;
+  }
+  for (let elm in hasmap) {
+    console.log(hasmap[elm]);
+    if (hasmap[elm] > 1) {
+      return false;
+    }
+  }
+  console.log(hasmap);
+  return true;
+}
+
+const arr1 = [1, 1, 1, 1, 1, 2, 3];
+const arr2 = [1, 3, -4, 5];
+console.log(areThereDuplicate(arr2));
+
+// using multiple pointer
+
+function isDuplicate(arr) {
+  let left = 0;
+  let second = left + 1;
+
+  while (left < arr.length) {
+    if (arr[left] === arr[second]) {
+      return true;
+    }
+    left++;
+    second++;
+  }
+  return false;
+}
+console.log(isDuplicate(arr2));
