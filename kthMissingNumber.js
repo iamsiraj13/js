f01f05985a874ac7379b58e126f80e045e64d243
iamsiraj13 <iamsiraj13@gmail.com>
2025-04-21 15:46:57 +0600

    kth missing number

--- kthMissingNumber.js
+++ kthMissingNumber.js
@@ -9,11 +9,45 @@ Example 1:
 
 Input: arr = [2,3,4,7,11], k = 5
 Output: 9
-Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...].
+ The 5th missing positive integer is 9.
 Example 2:
 
 Input: arr = [1,2,3,4], k = 2
 Output: 6
-Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6.
+Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing
+positive integer is 6.
  
  */
+
+// var findKthPositive = function (arr, k) {
+//   let lastIndex = arr.length - 1;
+//   let lastElm = arr[lastIndex];
+//   let miss = [];
+//   for (let i = 0; i <= lastElm; i++) {
+//     if (!arr.includes(i)) {
+//       miss.push(i);
+//     }
+//   }
+//   return miss[k];
+// };
+
+const arr = [1, 2, 3, 4],
+  k = 2;
+
+var findKthPositive = function (arr, k) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    let missing = arr[mid] - (mid + 1);
+    if (missing < k) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return left + k;
+};
+
+console.log(findKthPositive(arr, k));
