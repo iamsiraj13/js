cc92b78754667b1f5e06522c101bfdabc04f1919
iamsiraj13 <iamsiraj13@gmail.com>
2025-04-21 09:08:59 +0600

    binary search algorithm

---
+++ dsa/algorithm/binarySearch2.js
@@ -0,0 +1,23 @@
+// binary search / Divide and conquer
+// array must be sorted
+
+function searchElmIndex(arr, target) {
+  let low = 0;
+  let high = arr.length - 1;
+
+  while (low < high) {
+    let mid = Math.floor((low + high) / 2);
+
+    let midVal = arr[mid];
+
+    if (midVal === target) return mid;
+    if (target > midVal) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return -1;
+}
+
+console.log(searchElmIndex([1, 3, 5, 7, 8, 10], 9));
